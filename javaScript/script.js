3e7f7be233a8fb8986fa408d12e370df213ef5fc
duduzx02 <duduzx02@gmail.com>
2022-11-03 22:43:24 -0300

    Aula 04 - Pernonalizando o projeto

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,6 +1,8 @@
+let delay = -0.2;
 function createCard(date, day, games) {
+    delay = delay + 0.2
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
   <h2>${date}<span>${day}</span></h2>
 
   <ul>
@@ -20,32 +22,28 @@ function createGame(player1, hour, player2) {
     `;
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-<img src="assets/logo.svg" alt="Logo da Nlw da copa">
-</header>
-<main class="cards">
-    ${createCard(
-      "24/11",
-      "quinta",
-      createGame("switzerland", "07:00", "cameroon") +
-        createGame("uruguay", "10:00", "SouthKorea") +
-        createGame("portugal", "13:00", "ghana") +
-        createGame("brazil", "16:00", "serbia")
-    )}
-    ${createCard(
-      "28/11",
-      "segunda",
-      createGame("cameroon", "07:00", "serbia") +
+document.querySelector(".cards").innerHTML =
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "SouthKorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
       createGame("southKorea", "10:00", "ghana") +
-        createGame("brazil", "13:00", "switzerland") +
-        createGame("portugal", "16:00", "uruguay")
-    )}
-    ${createCard("02/12", "sexta", 
-    createGame("southKorea", "12:00", "portugal") + 
-    createGame("ghana", "12:00", "uruguay") + 
-    createGame("serbia", "16:00", "switzerland") + 
-    createGame("brazil", "16:00", "cameroon")
-    )}
-</main>
-`;
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("southKorea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguay") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("brazil", "16:00", "cameroon")
+  );
